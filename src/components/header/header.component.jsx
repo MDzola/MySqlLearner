29cce4111d3ad53c61a7d05d479df04544d2e6e3
Michael Drew Palazola <mpalazola@hotmail.com>
2020-03-16 13:59:21 -0500

    worked on header styling and homepage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,17 +6,27 @@ import './header.styles.scss'
 
 const Header = () => (
     <div className='header'>
+        <div className='container'>
         <Link  className='logo-container' to="/">
 
         </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SQL KeyWords
-            </Link>
-            <Link className='option' to='/shop'>
-                Tests
-            </Link>
-        </div>
+    </div>
+    <div class="items">
+    <div class="icon-wrapper">
+      <i class="fa fa-file-text-o"></i>
+    </div>
+    <div class="project-name">
+      <Link className='option' to='/keywords'><p>[KeyWords]</p></Link>
+    </div>
+  </div>
+  <div class="items">
+    <div class="icon-wrapper">
+      <i class="fa fa-file-text-o"></i>
+    </div>
+    <div class="project-name">
+      <Link className='option' to='/tests'><p>Tests</p></Link>
+    </div>
+  </div>
     </div>
 )
 
